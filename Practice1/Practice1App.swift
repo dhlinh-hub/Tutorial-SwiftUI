de0a5405044fd3c68d17715982499a194566583d
linhdh <doanhoai.linh1123@gmail.com>
2022-02-23 16:59:04 +0700

    Initial Commit

---
+++ Practice1/Practice1App.swift
@@ -0,0 +1,17 @@
+//
+//  Practice1App.swift
+//  Practice1
+//
+//  Created by Mac on 23/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Practice1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
